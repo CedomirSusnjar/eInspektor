43a0fda10ba7902e312217bb06049301cd3ca62b
mile348 <mile348348@gmail.com>
2020-01-05 13:38:42 +0100

    Model i veza na bazu.

---
+++ eInspektor/Database/database0.1.sql
@@ -0,0 +1,286 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema is-proj
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `is-proj` ;
+
+-- -----------------------------------------------------
+-- Schema is-proj
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `is-proj` DEFAULT CHARACTER SET utf8 ;
+USE `is-proj` ;
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`COMPANY`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`COMPANY` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`COMPANY` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `jib` INT(13) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `location` JSON NULL,
+  `owner` VARCHAR(255) NOT NULL,
+  `bookkeeper` VARCHAR(255) NULL,
+  `last_control` DATE NULL,
+  `last_control_type` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idCOMPANY_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`INSPECTOR`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`INSPECTOR` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`INSPECTOR` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `department` VARCHAR(255) NOT NULL,
+  `unavailable` INT NOT NULL DEFAULT 0,
+  `shift` INT NOT NULL,
+  `is_coordinator` TINYINT NOT NULL DEFAULT 0,
+  `username` VARCHAR(45) NOT NULL,
+  `salt` VARCHAR(45) NOT NULL,
+  `password_hash` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`VEHICLE`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`VEHICLE` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`VEHICLE` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `registration_num` VARCHAR(45) NOT NULL,
+  `max_capacity` INT NOT NULL DEFAULT 99,
+  `tag` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`ABSENCE`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`ABSENCE` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`ABSENCE` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `inspector_id` INT NOT NULL,
+  `date_from` DATE NOT NULL,
+  `date_to` DATE NULL,
+  `duration_days` INT NULL,
+  `absence_purpose` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_ABSENCE_INSPECTOR1_idx` (`inspector_id` ASC) VISIBLE,
+  CONSTRAINT `fk_ABSENCE_INSPECTOR1`
+    FOREIGN KEY (`inspector_id`)
+    REFERENCES `is-proj`.`INSPECTOR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`CONTROL`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`CONTROL` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`CONTROL` (
+  `id` INT NOT NULL,
+  `company_id` INT NOT NULL,
+  `start_date` DATE NOT NULL,
+  `finish_date` DATE NULL,
+  `is_finished` TINYINT NOT NULL DEFAULT 0,
+  `is_regular` TINYINT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_COMPANY_has_INSPECTOR_COMPANY1_idx` (`company_id` ASC) VISIBLE,
+  CONSTRAINT `fk_COMPANY_has_INSPECTOR_COMPANY1`
+    FOREIGN KEY (`company_id`)
+    REFERENCES `is-proj`.`COMPANY` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`VEHICLE_RESPONSIBILITY`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`VEHICLE_RESPONSIBILITY` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`VEHICLE_RESPONSIBILITY` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `vehicle_id` INT NOT NULL,
+  `inspector_id` INT NOT NULL,
+  `control_id` INT NULL,
+  `date` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_VEHICLE_has_CONTROL_VEHICLE1_idx` (`vehicle_id` ASC) VISIBLE,
+  INDEX `fk_VEHICLE_RESPONSIBILITY_INSPECTOR1_idx` (`inspector_id` ASC) VISIBLE,
+  INDEX `fk_VEHICLE_RESPONSIBILITY_CONTROL1_idx` (`control_id` ASC) VISIBLE,
+  CONSTRAINT `fk_VEHICLE_has_CONTROL_VEHICLE1`
+    FOREIGN KEY (`vehicle_id`)
+    REFERENCES `is-proj`.`VEHICLE` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_VEHICLE_RESPONSIBILITY_INSPECTOR1`
+    FOREIGN KEY (`inspector_id`)
+    REFERENCES `is-proj`.`INSPECTOR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_VEHICLE_RESPONSIBILITY_CONTROL1`
+    FOREIGN KEY (`control_id`)
+    REFERENCES `is-proj`.`CONTROL` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`REPORT`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`REPORT` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`REPORT` (
+  `control_id` INT NOT NULL,
+  `date` DATE NOT NULL,
+  `content` TEXT NOT NULL,
+  `control_justified` TINYINT NOT NULL,
+  PRIMARY KEY (`control_id`),
+  CONSTRAINT `fk_REPORT_CONTROL1`
+    FOREIGN KEY (`control_id`)
+    REFERENCES `is-proj`.`CONTROL` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`LAB_SAMPLE`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`LAB_SAMPLE` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`LAB_SAMPLE` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `company_id` INT NOT NULL,
+  `inspector_id` INT NOT NULL,
+  `date` DATE NOT NULL,
+  `description` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_LAB_SAMPLE_COMPANY1_idx` (`company_id` ASC) VISIBLE,
+  INDEX `fk_LAB_SAMPLE_INSPECTOR1_idx` (`inspector_id` ASC) VISIBLE,
+  CONSTRAINT `fk_LAB_SAMPLE_COMPANY1`
+    FOREIGN KEY (`company_id`)
+    REFERENCES `is-proj`.`COMPANY` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_LAB_SAMPLE_INSPECTOR1`
+    FOREIGN KEY (`inspector_id`)
+    REFERENCES `is-proj`.`INSPECTOR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`COMPLAINT`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`COMPLAINT` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`COMPLAINT` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `company_id` INT NOT NULL,
+  `text` TEXT NOT NULL,
+  `issuer` VARCHAR(255) NULL,
+  `is_justified` TINYINT NULL,
+  `date` DATE NOT NULL,
+  `control_id` INT NULL,
+  `is_resolved` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_COMPLAINT_COMPANY1_idx` (`company_id` ASC) VISIBLE,
+  INDEX `fk_COMPLAINT_CONTROL1_idx` (`control_id` ASC) VISIBLE,
+  CONSTRAINT `fk_COMPLAINT_COMPANY1`
+    FOREIGN KEY (`company_id`)
+    REFERENCES `is-proj`.`COMPANY` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_COMPLAINT_CONTROL1`
+    FOREIGN KEY (`control_id`)
+    REFERENCES `is-proj`.`CONTROL` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`CONTROL_has_INSPECTOR`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`CONTROL_has_INSPECTOR` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`CONTROL_has_INSPECTOR` (
+  `control_id` INT NOT NULL,
+  `inspector_id` INT NOT NULL,
+  PRIMARY KEY (`control_id`, `inspector_id`),
+  INDEX `fk_CONTROL_has_INSPECTOR_INSPECTOR1_idx` (`inspector_id` ASC) VISIBLE,
+  INDEX `fk_CONTROL_has_INSPECTOR_CONTROL1_idx` (`control_id` ASC) VISIBLE,
+  CONSTRAINT `fk_CONTROL_has_INSPECTOR_CONTROL1`
+    FOREIGN KEY (`control_id`)
+    REFERENCES `is-proj`.`CONTROL` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_CONTROL_has_INSPECTOR_INSPECTOR1`
+    FOREIGN KEY (`inspector_id`)
+    REFERENCES `is-proj`.`INSPECTOR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`DEPARTMENT`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`DEPARTMENT` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`DEPARTMENT` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `is-proj`.`JOINT_CONTROL`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `is-proj`.`JOINT_CONTROL` ;
+
+CREATE TABLE IF NOT EXISTS `is-proj`.`JOINT_CONTROL` (
+  `control_id` INT NOT NULL,
+  `department_id` INT NOT NULL,
+  INDEX `fk_JOINT_CONTROL_DEPARTMENT1_idx` (`department_id` ASC) VISIBLE,
+  INDEX `fk_JOINT_CONTROL_CONTROL1_idx` (`control_id` ASC) VISIBLE,
+  PRIMARY KEY (`control_id`),
+  CONSTRAINT `fk_JOINT_CONTROL_DEPARTMENT1`
+    FOREIGN KEY (`department_id`)
+    REFERENCES `is-proj`.`DEPARTMENT` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_JOINT_CONTROL_CONTROL1`
+    FOREIGN KEY (`control_id`)
+    REFERENCES `is-proj`.`CONTROL` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ eInspektor/DatabaseModel.cs
@@ -0,0 +1,158 @@
+namespace eInspektor
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DatabaseModel : DbContext
+    {
+        public DatabaseModel()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<absence> absences { get; set; }
+        public virtual DbSet<company> companies { get; set; }
+        public virtual DbSet<complaint> complaints { get; set; }
+        public virtual DbSet<control> controls { get; set; }
+        public virtual DbSet<department> departments { get; set; }
+        public virtual DbSet<inspector> inspectors { get; set; }
+        public virtual DbSet<lab_sample> lab_sample { get; set; }
+        public virtual DbSet<report> reports { get; set; }
+        public virtual DbSet<vehicle> vehicles { get; set; }
+        public virtual DbSet<vehicle_responsibility> vehicle_responsibility { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<absence>()
+                .Property(e => e.absence_purpose)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<company>()
+                .Property(e => e.name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<company>()
+                .Property(e => e.owner)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<company>()
+                .Property(e => e.bookkeeper)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<company>()
+                .Property(e => e.last_control_type)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<company>()
+                .HasMany(e => e.controls)
+                .WithRequired(e => e.company)
+                .HasForeignKey(e => e.company_id)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<company>()
+                .HasMany(e => e.complaints)
+                .WithRequired(e => e.company)
+                .HasForeignKey(e => e.company_id)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<company>()
+                .HasMany(e => e.lab_sample)
+                .WithRequired(e => e.company)
+                .HasForeignKey(e => e.company_id)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<complaint>()
+                .Property(e => e.text)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<complaint>()
+                .Property(e => e.issuer)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<control>()
+                .HasMany(e => e.complaints)
+                .WithOptional(e => e.control)
+                .HasForeignKey(e => e.control_id);
+
+            modelBuilder.Entity<control>()
+                .HasOptional(e => e.report)
+                .WithRequired(e => e.control);
+
+            modelBuilder.Entity<control>()
+                .HasMany(e => e.vehicle_responsibility)
+                .WithOptional(e => e.control)
+                .HasForeignKey(e => e.control_id);
+
+            modelBuilder.Entity<control>()
+                .HasMany(e => e.inspectors)
+                .WithMany(e => e.controls)
+                .Map(m => m.ToTable("control_has_inspector", "is-proj"));
+
+            modelBuilder.Entity<department>()
+                .Property(e => e.name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<inspector>()
+                .Property(e => e.name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<inspector>()
+                .Property(e => e.department)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<inspector>()
+                .Property(e => e.username)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<inspector>()
+                .Property(e => e.salt)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<inspector>()
+                .Property(e => e.password_hash)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<inspector>()
+                .HasMany(e => e.absences)
+                .WithRequired(e => e.inspector)
+                .HasForeignKey(e => e.inspector_id)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<inspector>()
+                .HasMany(e => e.lab_sample)
+                .WithRequired(e => e.inspector)
+                .HasForeignKey(e => e.inspector_id)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<inspector>()
+                .HasMany(e => e.vehicle_responsibility)
+                .WithRequired(e => e.inspector)
+                .HasForeignKey(e => e.inspector_id)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<lab_sample>()
+                .Property(e => e.description)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<report>()
+                .Property(e => e.content)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<vehicle>()
+                .Property(e => e.registration_num)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<vehicle>()
+                .Property(e => e.tag)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<vehicle>()
+                .HasMany(e => e.vehicle_responsibility)
+                .WithRequired(e => e.vehicle)
+                .HasForeignKey(e => e.vehicle_id)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
