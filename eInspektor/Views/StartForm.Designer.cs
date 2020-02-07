07ae585f207680efec8dcfe38ce6601b90bbc948
mile348 <mile348348@gmail.com>
2020-02-07 17:11:27 +0100

    Popravka konflikta.

--- eInspektor/Views/StartForm.Designer.cs
+++ eInspektor/Views/StartForm.Designer.cs
@@ -37,108 +37,18 @@ namespace eInspektor
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.odjavaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.menuStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.odjavaToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // odjavaToolStripMenuItem
-            // 
-            this.odjavaToolStripMenuItem.Name = "odjavaToolStripMenuItem";
-            this.odjavaToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
-            this.odjavaToolStripMenuItem.Text = "Odjava";
-            this.odjavaToolStripMenuItem.Click += new System.EventHandler(this.odjavaToolStripMenuItem_Click);
-            // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Image = global::eInspektor.Properties.Resources.automobile;
-            this.button1.Location = new System.Drawing.Point(269, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 205);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Vozila";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Image = global::eInspektor.Properties.Resources.calendar;
-            this.button2.Location = new System.Drawing.Point(3, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(260, 205);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Sedmični plan";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button3.Location = new System.Drawing.Point(535, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(262, 205);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Inspektori";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Image = global::eInspektor.Properties.Resources.customer;
-            this.button4.Location = new System.Drawing.Point(3, 214);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(260, 205);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Reklamacije";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button5.Image = global::eInspektor.Properties.Resources.stats;
-            this.button5.Location = new System.Drawing.Point(269, 214);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(260, 205);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Izvještaji";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button6.Image = global::eInspektor.Properties.Resources.store;
-            this.button6.Location = new System.Drawing.Point(535, 214);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(262, 205);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Subjekti";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -161,6 +71,102 @@ namespace eInspektor
             this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 422);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Image = global::eInspektor.Properties.Resources.store;
+            this.button6.Location = new System.Drawing.Point(535, 214);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(262, 205);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Subjekti";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            button6Image = button6.Image;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Image = global::eInspektor.Properties.Resources.stats;
+            this.button5.Location = new System.Drawing.Point(269, 214);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(260, 205);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Izvještaji";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            button5Image = button5.Image;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Image = global::eInspektor.Properties.Resources.customer;
+            this.button4.Location = new System.Drawing.Point(3, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(260, 205);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Reklamacije";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            button4Image = button4.Image;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(535, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(262, 205);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Inspektori";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            button3Image = button3.Image;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Image = global::eInspektor.Properties.Resources.calendar;
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 205);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sedmični plan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            button2Image = button2.Image;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Image = global::eInspektor.Properties.Resources.automobile;
+            this.button1.Location = new System.Drawing.Point(269, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 205);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vozila";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1Image = button1.Image;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.odjavaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // odjavaToolStripMenuItem
+            // 
+            this.odjavaToolStripMenuItem.Name = "odjavaToolStripMenuItem";
+            this.odjavaToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.odjavaToolStripMenuItem.Text = "Odjava";
+            this.odjavaToolStripMenuItem.Click += new System.EventHandler(this.odjavaToolStripMenuItem_Click);
+            // 
             // StartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -174,23 +180,24 @@ namespace eInspektor
             this.Text = "Koordinator";
             this.Load += new System.EventHandler(this.StartForm_Load);
             this.Resize += new System.EventHandler(this.StartForm_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem odjavaToolStripMenuItem;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
